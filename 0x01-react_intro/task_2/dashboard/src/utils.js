58a69dc13b17901376bc2eecb7310e489cccdd04
mukarama-farouk <faroukmukarama@gmail.com>
2024-01-30 22:05:01 +0000

    updates

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -11,3 +11,9 @@ export function getFooterCopy(isIndex) {
     }
 
 }
+
+export function getLatestNotification() {
+    return (
+        "<strong>Urgent requirement</strong> - complete by EOD"
+    )
+}
