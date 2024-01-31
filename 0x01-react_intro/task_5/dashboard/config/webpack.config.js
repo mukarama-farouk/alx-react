6548764be9d470ee6334c2dfbe1006d81109eceb
mukarama-farouk <faroukmukarama@gmail.com>
2024-01-31 08:30:04 +0000

    task-6

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist'),
     },
     mode: "production",
     module: {
