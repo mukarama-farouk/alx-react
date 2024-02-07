c82e09beb4683e54dd3a9ebe6930ed541271c388
mukarama-farouk <faroukmukarama@gmail.com>
2024-02-07 09:07:35 +0000

    updates

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -17,7 +17,7 @@ const listCourses = [
 const listNotifications = [
   { id: 1, type: "default", value: "New course available" },
   { id: 2, type: "urgent", value: "New resume available" },
-  { id: 3, type: "urgent", html: getLatestNotification() },
+  { id: 3, type: "urgent", html: listNotifications },
 ];
 
 function App({isLoggedIn}) {
