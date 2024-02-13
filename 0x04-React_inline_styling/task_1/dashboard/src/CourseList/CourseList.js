d000e3a2ec0bee8592569c526f05c8edfc2433c9
mukarama-farouk <faroukmukarama@gmail.com>
2024-02-13 10:15:54 +0000

    update

--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -1,51 +1,60 @@
-import React from 'react';
-import CourseListRow from './CourseListRow';
-import CourseShape from './CourseShape';
-import './CourseList.css'
-import PropTypes from 'prop-types';
-import { StyleSheet, css } from 'aphrodite';
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+import CourseListRow from "./CourseListRow";
+import PropTypes from "prop-types";
+import CourseShape from "./CourseShape";
 
-const listCourses = [
-    { id: 1, name: "ES6", credit: 60 },
-    { id: 2, name: "Webpack", credit: 20 },
-    { id: 3, name: "React", credit: 40 },
-];
-
-const styles = StyleSheet.create({
-    table: {
-        marginTop: "2em",
-        width: "100%",
-        border: "1px solid #ddd",
-        fontSize: "1.2rem",
-        marginBottom: "15em",
-        marginLeft: "auto",
-        marginRight: "auto",
-    }, 
-})
-
-function CourseList() {
-    return (
-        <table id='CourseList'className={css(styles.table)} >
-            <thead>
-                <CourseListRow textFirstCell="Available courses" textSecondCell={null} isHeader={true} />
-                <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true}  />
-            </thead>
-            <tbody>
-                {listCourses.length > 0 ? (
-                    listCourses.map(({ id, name, credit }) => <CourseListRow key={id} textFirstCell={name} textSecondCell={credit} />)
-                ) : (
-                <CourseListRow textFirstCell="No course available yet" />
-                )}
-            </tbody>
-        </table>
-    )
+function CourseList({ listCourses }) {
+  return (
+    <table id="CourseList" className={css(styles.table)}>
+      <thead>
+        <CourseListRow textFirstCell="Available courses" isHeader={true} />
+        <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+      </thead>
+      <tbody>
+        {listCourses.length > 0 ? (
+          listCourses.map(({ id, name, credit }) => <CourseListRow key={id} textFirstCell={name} textSecondCell={credit} />)
+        ) : (
+          <CourseListRow textFirstCell="No course available yet" />
+        )}
+      </tbody>
+    </table>
+  );
 }
 
+const styles = StyleSheet.create({
+  table: {
+    marginTop: "2em",
+    width: "100%",
+    border: "1px solid #ddd",
+    fontSize: "1.2rem",
+    marginBottom: "15em",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+
+  th: {
+    borderBottom: "1px solid #ddd",
+    width: "80%",
+  },
+
+  td: {
+    width: "80%",
+  },
+
+  tr: {
+    "nth-child(2)": {
+      textAlign: "left",
+    },
+  },
+});
+
 CourseList.propTypes = {
-    listCourses: PropTypes.arrayOf(CourseShape),
-  };
-  
-  CourseList.defaultProps = {
-    listCourses: [],
+  listCourses: PropTypes.arrayOf(CourseShape),
 };
+
+CourseList.defaultProps = {
+  listCourses: [],
+};
+
 export default CourseList;
--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import holberton_logo from '../assets/Holberton-logo.jpg';
-import { StyleSheet, css } from 'aphrodite';
-
-const styles = StyleSheet.create({
-    "App-header" :{
-        marginTop: "150px",
-        display: "flex",
-        alignItems: "center",
-        color: "#e01d3f",
-        fontSize: "20px",
-        borderBottom: "3px solid #e01d3f",
-        width: "100%"
-      },
-    
-      img : {
-        width: "200px"
-      
-      }
-})
+import React from "react";
+import logo from "../assets/holberton-logo.jpg";
+import { StyleSheet, css } from "aphrodite";
 
 function Header() {
-    return (
-        <div className={css(styles['App-header'])}>
-            <img src={holberton_logo}  className={css(styles.img)} alt="logo" />
-            <h1>School dashboard</h1>
-        </div>
-    )
+  return (
+    <>
+      <div className={css(styles["App-header"])}>
+        <img src={logo} className={css(styles.img)} alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  "App-header": {
+    fontSize: "1.4rem",
+    color: "#e0354b",
+    display: "flex",
+    alignItems: "center",
+    borderBottom: "3px solid #e0354b",
+  },
+
+  img: {
+    width: "200px",
+    height: "200px",
+  },
+});
+
 export default Header;
