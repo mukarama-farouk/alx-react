f4c7178c4449a2837aad1d5918958c4c9492419d
mukarama-farouk <faroukmukarama@gmail.com>
2024-02-13 13:50:53 +0000

    task 3

---
+++ 0x04-React_inline_styling/task_3/dashboard/src/Login/Login.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Login from './Login';
+import { shallow } from 'enzyme';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe('Login component', () => {
+    it('should render without crashing', () => {
+      const wrapper = shallow(<Login />);
+      expect(wrapper.exists()).toBe(true);
+    });
+  
+    it('should have  2 input and 2 label tags', () => {
+      const wrapper = shallow(<Login />);
+      expect(wrapper.find('input')).toHaveLength(2);
+      expect(wrapper.find('label')).toHaveLength(2);
+    });
+});
