9b85c9830be6d22de369890a4c112235110caf6b
mukarama-farouk <faroukmukarama@gmail.com>
2024-02-21 17:37:07 +0000

    task 2

---
+++ 0x05-react_state/task_2/dashboard/src/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import holberton_logo from '../assets/Holberton-logo.jpg';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+    "App-header" :{
+        marginTop: "150px",
+        display: "flex",
+        color: "#e01d3f",
+        fontSize: "20px",
+        borderBottom: "3px solid #e01d3f",
+        width: "100%"
+      },
+    
+      img : {
+        width: "200px"
+      
+      }
+})
+
+function Header() {
+    return (
+        <div className={css(styles['App-header'])}>
+            <img src={holberton_logo}  className={css(styles.img)} alt="logo" />
+            <h1>School dashboard</h1>
+        </div>
+    )
+}
+export default Header;
