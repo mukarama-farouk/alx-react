043aaa69f5d30d36a1f1bc5dcfa75f6e285ff0df
mukarama-farouk <faroukmukarama@gmail.com>
2024-02-21 09:34:47 +0000

    task 1 - Login component

--- 0x05-react_state/task_1/dashboard/src/Login/Login.test.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.test.js
@@ -22,4 +22,14 @@ describe('Login component', () => {
       expect(wrapper.find('input')).toHaveLength(2);
       expect(wrapper.find('label')).toHaveLength(2);
     });
+
+    it("verify that the submit button is disabled by default", () => {
+      expect(wrapper.find("input[type='submit']").props().disabled).toEqual(true);
+    });
+  
+    it("verify that after changing the value of the two inputs, the button is enabled", () => {
+      wrapper.find("#email").simulate('change', {target: {value: 't'}});
+      wrapper.find("#password").simulate('change', {target: {value: 't'}});
+      expect(wrapper.find("input[type='submit']").props().disabled).toEqual(false);
+    });
 });
